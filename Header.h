547af80bbfe93411aeff5f9eacd88f31b382a3b6
savvita <Savc_ea11@student.itstep.org>
2021-12-19 23:10:20 +0200

    add testing

---
+++ Header.h
@@ -0,0 +1,7 @@
+#pragma once
+#include<iostream>
+#include"Date.h"
+#include"Human.h"
+#include"Employee.h"
+#include"Schoolchild.h"
+#include"Student.h"
--- prog.cpp
+++ prog.cpp
@@ -1,8 +1,17 @@
-#include<iostream>
-#include"Employee.h"
-#include"Student.h"
+#include"Header.h"
 
 int main()
 {
+	Human h;
+	std::cout << h;
+	h.setName("Name");
+	h.setBirthday(Date(1, 1, 1900));
+	std::cout << h;
 
+	Student st("Name2", Date(2, 2, 2000), 100);
+	std::cout << st;
+	Schoolchild sc("Name3", Date(3, 3, 1500), 5.6);
+	std::cout << sc;
+	Employee e;
+	std::cout << e;
 }
